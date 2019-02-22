4e559c2a50ad18bb9618ff4c3840c81200cacc7b
E-Drop <erosendo.developer@gmail.com>
2019-02-22 14:57:30 +0100

    add some css and js animations

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function main () {
-  console.log('JS Running');
+  document.getElementById('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+        this.classList.add('away');
+        setTimeout(() => {
+            document.getElementById('form-content').remove();
+            var submitButton = document.getElementById('submit');
+            submitButton.classList.add('message');
+            var text = submitButton.firstChild;
+        
+            text.nodeValue = "Su mensaje ha sido entregado";
+            this.classList.remove('away');
+        }, 5000);
+  });
 }
 
 window.addEventListener('load', main);
